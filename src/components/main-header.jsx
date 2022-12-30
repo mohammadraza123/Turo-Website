f39e731ab1f7f995e25e95bbe36ba3d5993222f1
Muhammad Raza <103913857+mohammadraza123@users.noreply.github.com>
2022-12-30 19:24:26 +0500

    Add files via upload

---
+++ src/components/main-header.jsx
@@ -0,0 +1,34 @@
+// import React from 'react';
+
+// function Main(){
+//     return(
+//         <>
+//         <div className='dropdown-date'>
+
+//     <div className='formm'>
+//         <label>Where</label>
+//         <br></br>
+//         <input type={'text'} placeholder="City, airport,address or hotel" style={{width: "320px"}} ></input>
+//     </div>
+
+//     <div className='form'>
+//         <label>From</label>
+//         <br></br>
+//         <input type={'date'}></input>
+//         <input type={'time'}></input>
+// </div>
+// <div className='form'>
+//         <label>Until</label>
+//         <br></br>
+//         <input type={'date'}></input>
+//         <input type={'time'}></input>
+// </div>
+//         </div>
+    
+    
+// </>
+//     )
+// }
+//     export default Main;
+
+  
